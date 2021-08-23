c3a75080798dc420c11d89bcd6216ac2ee9c4d97
shaunZh <308826842@qq.com>
2021-08-23 20:12:22 +0800

    update: 使用html-webpack-plugin和webpack-dev-server

--- webpack.config.js
+++ webpack.config.js
@@ -3,10 +3,14 @@
  * @Author: Hexon
  * @Date: 2021-08-23 18:44:17
  * @LastEditors: Hexon
- * @LastEditTime: 2021-08-23 18:58:53
+ * @LastEditTime: 2021-08-23 20:05:52
  */
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const dev = process.env.NODE_ENV === "development";
+const config = require("./public/config")[dev ? "dev" : "build"];
+
 module.exports = {
-  mode: "development",
+  mode: dev ? "development" : "production",
   module: {
     rules: [
       {
@@ -16,4 +20,16 @@ module.exports = {
       },
     ],
   },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "./public/index.html",
+      filename: "index.html",
+      config: config.template,
+      // minify: {
+      //   removeAttributeQuotes: false, //是否删除属性的双引号
+      //   collapseWhitespace: false, //是否折叠空白
+      // },
+      // hash: true,
+    }),
+  ],
 };
