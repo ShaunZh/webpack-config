c56b6d13812a307f2f18d06f346f371afa135c25
shaunZh <308826842@qq.com>
2021-08-23 19:06:26 +0800

    update: 使用webpack的mode，配置运行模式

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@
  * @Author: Hexon
  * @Date: 2021-08-20 18:24:36
  * @LastEditors: Hexon
- * @LastEditTime: 2021-08-20 18:24:52
+ * @LastEditTime: 2021-08-23 18:59:53
  */
 
 //index.js
@@ -15,5 +15,7 @@ class Animal {
     return this.name;
   }
 }
+const constVariable = "const";
+console.log("constVariable: ", constVariable);
 
 const dog = new Animal("dog");
--- webpack.config.js
+++ webpack.config.js
@@ -3,9 +3,10 @@
  * @Author: Hexon
  * @Date: 2021-08-23 18:44:17
  * @LastEditors: Hexon
- * @LastEditTime: 2021-08-23 18:48:43
+ * @LastEditTime: 2021-08-23 18:58:53
  */
 module.exports = {
+  mode: "development",
   module: {
     rules: [
       {
